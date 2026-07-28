195c3a789cb678e601e09536f77da08770d7a8ba
sartaj sadat <sartajsadat65@gmail.com>
2026-07-28 10:59:48 +0430

    starting new project

---
+++ Backend/src/main/java/Agriculture/AgricultureProject/AgricultureProjectApplication.java
@@ -0,0 +1,13 @@
+package Agriculture.AgricultureProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricultureProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgricultureProjectApplication.class, args);
+	}
+
+}
